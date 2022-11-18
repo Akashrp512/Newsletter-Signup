822e0c07a617ec7cbcbcc89070cb7016d1dc91a0
akash <akashrp512@gmail.com>
2022-11-18 11:52:01 +0530

    commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const port = 3000;
+const app = express();
+
+
+
+app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`);
+});
